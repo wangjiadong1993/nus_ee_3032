4629a7d42f9824394f3d3ac2f9c73780fe228dd7
Wang Jiadong <wangjiadong1993@gmail.com>
2015-03-01 21:34:40 +0800

    bt failure caused by print, fixed

--- LPC1769_bluetooth/src/main.c
+++ LPC1769_bluetooth/src/main.c
@@ -81,45 +81,58 @@ void init_uart(void)
 
 void UART3_IRQHandler(void)
 {
-	//printf("interrupted\n");
 	uint8_t roo = 0;
 	UART_Receive(LPC_UART3,&roo,1, BLOCKING);
-	printf("%c", roo);
-	printf("");
+	str[num] = roo;
+	num++;
+	str[num] = '\0';
 }
-
-
 // ****************
 int main(void) {
 	
 
 	initial_gpio();
-
 	init_uart();
-	//char* moo= "U, 9600, N";
-	uint8_t roo;
-	uint8_t i;
-	char str[100]="0";
-	uint32_t num=0;
-	//moo="AT+PIN0011";
-	//NVIC_EnableIRQ(UART3_IRQn);
-
-
+//	char* moo= "U, 9600, N";
+//	uint8_t roo;
+//	uint8_t i;
+//	char str[100]="0";
+//	uint32_t num=0;
+//	moo="AT+PIN0011";
+	NVIC_EnableIRQ(UART3_IRQn);
+	if (SysTick_Config(SystemCoreClock / 1000)) {
+		while (1);  // Capture error
+	}
 	while(1)
 	{
-		while(LPC_UART3->LSR&0x1)
-		{
-			roo = LPC_UART3->RBR;
-			str[num] = roo;
-			num++;
-			//printf("%c", roo);
-		}
-		//str[num]='\0';
-		printf("%s", str);
-		//num=0;
-		for(i = 0; i<num; i++)
-			printf("%c", str[num]);
-		num=0;
-		//printf("data finished\n");
+		;
+//		for
+//		UART_Send(LPC_UART3, );
+//		if(strlen(str) >=100)
+//		{
+//			str[0]="\0";
+//			num=0;
+//
+//		}
+//		systick_delay(200);
+//		if(strlen(str) != 0)
+//		{
+//			printf("%s\n", str);
+//			str[0]='\0';
+//		}
+////		while(LPC_UART3->LSR&0x1)
+////		{
+////			roo = LPC_UART3->RBR;
+////			str[num] = roo;
+////			num++;
+//			//printf("%c", roo);
+//		}
+//		//str[num]='\0';
+////		printf("%s", str);
+//		//num=0;
+////		for(i = 0; i<num; i++)
+////			printf("%c", str[num]);
+//		//num=0;
+////		//printf("data finished\n");
 	}
 }
