7226fc7ec357ca8728794aa5fef0ff59ebbf4f1f
Wang Jiadong <wangjiadong1993@gmail.com>
2015-03-26 15:22:30 +0800

    added in the sleep status

--- EE3032_Project_Main/inc/Cons_3032.h
+++ EE3032_Project_Main/inc/Cons_3032.h
@@ -13,7 +13,7 @@
 #define LOAD_FILE "/load.txt"
 #define LOCATE_FILE "/location.txt"
 #define LOAD_FILE_TEST "/load_test.txt"
-#define GPS_TIMER_LIMIT 60000
+#define GPS_TIMER_LIMIT 600
 //#define LOAD_TIMER_LIMIT
 int load_write(int front, int middle, int rear, int temp)
 {
@@ -80,3 +80,4 @@ void clean_up_files()
 {
 	fl_remove(LOAD_FILE);
 }
+
